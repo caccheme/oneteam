598c133ba8a4002b6905ef2f851a2c01ef047ad0
Charity Howell <caccheme@hotmail.com>
2013-01-30 10:16:01 -0600

    remove cal files

--- app/controllers/calendars_controller.rb
+++
@@ -1,9 +0,0 @@
-class CalendarsController < ApplicationController
-
-def index
-  @requests = Request.all
-  @requests_by_date = @requests.group_by(&:start_date)
-  @date = params[:date] ? Date.parse(params[:date]) : Date.today
-end
-
-end
